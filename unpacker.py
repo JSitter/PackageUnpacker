227cc6ff0b750e6ce9043bbff247f22abbc49050
Justin Sitter <justin@jaytria.com>
2020-08-22 21:14:13 -0700

    Add unzip function

--- unpacker.py
+++ unpacker.py
@@ -1,45 +1,60 @@
 #!/usr/bin/env python3
 import sys
 import zipfile
+import gzip
 from os import path
 from optparse import OptionParser
 
-usage = "usage: %prog [options] zipped_package_location destination_package_location"
-parser = OptionParser(usage=usage)
-parser.add_option("-d",
-                    help="Extract project into existing directory.",
-                    action="store_true",
-                    dest="directory",
-                    default=False)
+def unpack_zip(source, destination):
+    zipReference = zipfile.ZipFile(source, 'r')
+    zipReference.extractall(destination)
+    zipReference.close()
 
-(options, args) = parser.parse_args()
+def unpack_gz(source, destination):
+    print("Unpack gz")
 
-print("option parser", options.directory)
-print("option parser args", args)
+def unpack_zip_into(source, destination):
+    print("Unpack into")
 
-home_directory = path.dirname(path.realpath(__file__))
+def unpack_gz_into(source, destination):
+    print("Unpack gz into")
 
-if len(args) > 1:
-    final_project_path = home_directory+"/"+args[1]
-    zipped_project_path = home_directory+"/"+args[0]
+if __name__ == "__main__":
+    usage = "usage: %prog [options] zipped_package_location destination_package_location"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-d",
+                        help="Extract project into existing directory.",
+                        action="store_true",
+                        dest="directory",
+                        default=False)
 
-else:
-    print("Error: 2 arguments required {} provided".format(len(sys.argv)-1))
-    exit()
+    (options, args) = parser.parse_args()
+    home_directory = path.dirname(path.realpath(__file__))
 
-if not path.exists(zipped_project_path):
-    print("Error: Zipped Project doesn't exist")
-    exit()
+    if len(args) > 1:
+        final_project_path = home_directory+"/"+args[1]
+        zipped_project_path = home_directory+"/"+args[0]
+    else:
+        raise Exception("Error: 2 arguments required {} provided".format(len(sys.argv)-1))
 
-ext = zipped_project_path.split('.')[-1]
+    if not path.exists(zipped_project_path):
+        raise Exception("Error: Zipped Project doesn't exist")
 
-if (ext != "zip" ) and (ext != "gz"):
-    print("Error: Unknown project file format. Must be zip or gz")
-    exit()
+    # May not be needed -- should rely on errors thrown by zipping package
+    ext = zipped_project_path.split('.')[-1]
+    if (ext != "zip" ) and (ext != "gz"):
+        raise Exception("Error: Unknown project file format. Must be zip or gz")
+    if options.directory:
+        if not path.exists(final_project_path):
+            raise Exception("Error: Destination directory does not exist.")
 
-if options.directory:
-    if not path.exists(final_project_path):
-        print("Error: Destination directory does not exist.")
-        exit()
-
-print("Hello all")
+    if options.directory:
+        if ext == "zip":
+            unpack_zip_into(zipped_project_path, final_project_path)
+        else:
+            unpack_gz_into(zipped_project_path, final_project_path)
+    else:
+        if ext == "zip":
+            unpack_zip(zipped_project_path, final_project_path)
+        else:
+            unpack_gz(zipped_project_path, final_project_path)
