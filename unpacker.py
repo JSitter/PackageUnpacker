35b36cc18c36c74fc489c1eacc824e2e02bdebe4
Justin Sitter <justin@jaytria.com>
2020-08-22 20:01:00 -0700

    Parse option flags

--- unpacker.py
+++ unpacker.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
 import sys
 import zipfile
-import os
+from os import path
+from optparse import OptionParser
 
-home_directory = os.path.dirname(os.path.realpath(__file__))
+usage = "usage: %prog [options] zipped_package_location destination_package_location"
+parser = OptionParser(usage=usage)
+parser.add_option("-d",
+                    help="Extract project into existing directory.",
+                    action="store_true",
+                    dest="directory",
+                    default=False)
 
-if len(sys.argv) > 1:
-    project_directory = sys.argv[1]
-    project_path = home_directory+"/"+sys.argv[1]
+(options, args) = parser.parse_args()
+
+print("option parser", options.directory)
+print("option parser args", args)
+
+home_directory = path.dirname(path.realpath(__file__))
+
+if len(args) > 1:
+    final_project_path = home_directory+"/"+args[1]
+    zipped_project_path = home_directory+"/"+args[0]
 
 else:
-    print("ERROR: Project Directory Not Specified")
+    print("Error: 2 arguments required {} provided".format(len(sys.argv)-1))
     exit()
 
-print("project directory", project_directory)
+if not path.exists(zipped_project_path):
+    print("Error: Zipped Project doesn't exist")
+    exit()
+
+ext = zipped_project_path.split('.')[-1]
+
+if (ext != "zip" ) and (ext != "gz"):
+    print("Error: Unknown project file format. Must be zip or gz")
+    exit()
+
+if options.directory:
+    if not path.exists(final_project_path):
+        print("Error: Destination directory does not exist.")
+        exit()
+
+print("Hello all")
